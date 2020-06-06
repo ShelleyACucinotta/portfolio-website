77abc5cec03b2eb642db711ad340b76433bfc32b
ShelleyACucinotta <62570124+ShelleyACucinotta@users.noreply.github.com>
2020-06-06 14:23:15 -0600

    Updated

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ console.log("Welcome to my portfolio website!");
 
 
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'navtoggle';
-  }
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
 }
