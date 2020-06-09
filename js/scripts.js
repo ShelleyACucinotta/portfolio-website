37b169687fddb8355d9c87dcd44c48290deb82ba
ShelleyACucinotta <62570124+ShelleyACucinotta@users.noreply.github.com>
2020-06-09 11:06:38 -0600

    Updated js/scripts.js
    Changed " " to ' '
    Adjusted indents

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-console.log("Welcome to my portfolio website!");
-
+console.log('Welcome to my portfolio website!');
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-    } else {
-        x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
